1c3984bbcf176df3a6c44e02be90f4f5f5fa061e
Laura Beleño <vaneessa10@gmail.com>
2022-09-25 13:55:38 +0200

    ejercicio 10 clase

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,27 @@
 let todos = []
 
+const removeTodo = (todoEl) => {
+    const todoIndex = todos.findIndex((todo) => {
+        return todo.toLowerCase() === todoEl.textContent.toLowerCase()
+    })
+    if (todoIndex > -1) {
+        todos.splice(todoIndex, 1)
+    }
+}
+    
 const renderTodos = (todos) => {
     const todoList = document.querySelector("#todos")
     todoList.innerHTML =  " "
 
-    todos.forEach((todo) => {
-            todoList.appendChild(generateTodoDOM(todo))
-    })
-    
+    if (todos.length > 0) {
+        todos.forEach((todo) => {todoList.appendChild(generateTodoDOM(todo))})
+    } else {
+        const messageEl = document.createElement('p')
+        messageEl.classList.add('empty-message')
+        messageEl.textContent = 'There are no todos to show'
+        todoList.appendChild(messageEl)
+    }
+
 }
 
 const generateTodoDOM = (todo) => {
@@ -22,6 +36,15 @@ const generateTodoDOM = (todo) => {
     containerEl.classList.add('list-item__container')
     todoEl.appendChild(containerEl)
 
+    const removeButton = document.createElement('button')
+    removeButton.textContent = 'remove'
+    removeButton.classList.add('button', 'button--text')
+    todoEl.appendChild(removeButton)
+    removeButton.addEventListener('click', () => {
+        removeTodo(todoText)
+        renderTodos(todos)
+    })
+
     return todoEl
 }
 
@@ -34,11 +57,11 @@ const createTodo = (text) => {
 document.querySelector('#new-todo').addEventListener('submit', (e) => {
     e.preventDefault()
     const text = e.target.elements.text.value.trim()
+    console.log("texto añadido: " + text)
 
-    if (text.length > 0) {
+     if (text.length > 0) {
         createTodo(text)
         renderTodos(todos)
-    }
-
-    console.log(todos)
+        }
+    console.log(todos);
 })
